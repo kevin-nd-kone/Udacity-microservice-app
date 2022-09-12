af36d81b58d03170eae9a10d172612e3bdd3507b
kevinDlan <kevinkone19@gmail.com>
2022-09-12 00:28:36 +0000

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=microservice
+export POSTGRES_PASSWORD=kevinKONE
+export POSTGRES_HOST=microservice-instance.c8enrw1n3di4.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::microservice-211972899387-dev
+export AWS_REGION=us-west-1
+export AWS_PROFILE=microservice-user
+export JWT_SECRET=testing
+export URL=http://localhost:8100
