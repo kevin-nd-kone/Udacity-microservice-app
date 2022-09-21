6b828d02dc2d4f6d5619cfbff64cf96508582702
kevinDlan <kevinkone19@gmail.com>
2022-09-21 21:09:08 +0000

    create kubernetes deployment file

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=microservice
 export POSTGRES_PASSWORD=kevinKONE
-export POSTGRES_HOST=microservice-instance.c8enrw1n3di4.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=microservice.ctbl441qu9hd.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::microservice-211972899387-dev
-export AWS_REGION=us-west-1
+export AWS_REGION=us-east-2
 export AWS_PROFILE=microservice-user
 export JWT_SECRET=testing
 export URL=http://localhost:8100
